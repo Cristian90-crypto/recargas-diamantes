02938bc2c3847d9bb4c4ec11315357ed34dc245c
Cristian90-crypto <sofyamontes99@gmail.com>
2026-08-14 16:00:51 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Recargas de Diamantes</h1>
+      <p>Tu página de recargas está comenzando.</p>
+    </div>
+  );
+}
+
+export default App;
