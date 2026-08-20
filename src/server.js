41686522b6c152ea4b0c94f7fc83e48099ca4de0
Cristian90-crypto <sofyamontes99@gmail.com>
2026-08-20 17:40:32 -0400

    Actualizar server.js

--- src/server.js
+++ src/server.js
@@ -2,6 +2,17 @@ import express from "express";
 import crypto from "crypto";
 
 const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, X-FT-API-ID, X-FT-Timestamp, X-FT-Nonce, X-FT-Signature");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 
 const PORT = process.env.PORT || 10000;
 
